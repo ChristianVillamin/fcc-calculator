921b0f577e0e129dba3108c354f2f77f493ff6d3
Christian Villamin <ChristianVillamin31@gmail.com>
2019-07-01 16:57:00 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+  const [equation, setEquation] = React.useState('');
+  const handleClick = e => {};
+
+  return (
+    <>
+      <div id="calculator">
+        <div onClick={handleClick} className="result" id="equal">
+          =
+        </div>
+        <div onClick={handleClick} className="digitZero" id="zero">
+          0
+        </div>
+        <div onClick={handleClick} className="digit" id="one">
+          1
+        </div>
+        <div onClick={handleClick} className="digit" id="two">
+          2
+        </div>
+        <div onClick={handleClick} className="digit" id="three">
+          3
+        </div>
+        <div onClick={handleClick} className="digit" id="four">
+          4
+        </div>
+        <div onClick={handleClick} className="digit" id="five">
+          5
+        </div>
+        <div onClick={handleClick} className="digit" id="six">
+          6
+        </div>
+        <div onClick={handleClick} className="digit" id="seven">
+          7
+        </div>
+        <div onClick={handleClick} className="digit" id="eight">
+          8
+        </div>
+        <div onClick={handleClick} className="digit" id="nine">
+          9
+        </div>
+        <div onClick={handleClick} className="operation" id="add">
+          +
+        </div>
+        <div onClick={handleClick} className="operation" id="subtract">
+          -
+        </div>
+        <div onClick={handleClick} className="operation" id="multiply">
+          X
+        </div>
+        <div onClick={handleClick} className="operation" id="divide">
+          /
+        </div>
+        <div onClick={handleClick} className="dot" id="decimal">
+          .
+        </div>
+        <div onClick={handleClick} className="ac" id="clear">
+          AC
+        </div>
+        <div id="display" className="output">
+          Display
+        </div>
+      </div>
+    </>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
